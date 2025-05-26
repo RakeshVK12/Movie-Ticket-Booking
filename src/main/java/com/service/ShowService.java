8776a4f2d82568b72ff18e93af55e117c3d00c51
RakeshVK12 <rakeshtpr2000@gmail.com>
2025-05-26 22:53:42 +0530

    Second commit

--- src/main/java/com/service/ShowService.java
+++ src/main/java/com/service/ShowService.java
@@ -1,5 +1,17 @@
 package com.service;
 
-public class ShowService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.repository.BookingRepository;
+import com.repository.ShowRepository;
+
+@Service
+public class ShowService {
+	
+	@Autowired
+	BookingRepository repo;
+	
+	@Autowired
+	ShowRepository showRepo;
 }
